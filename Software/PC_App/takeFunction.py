8b4001b61c2ffda40bd976ebc2871d5cdd5f810b
Christian Loya <91439992+ElBroderGTX@users.noreply.github.com>
2023-05-12 19:36:19 -0600

    callFunction class con estructura

--- Software/PC_App/takeFunction.py
+++ Software/PC_App/takeFunction.py
@@ -6,20 +6,24 @@ class callFunction:
     """ Class to receive and evaluate function """
     def __init__(self, name:str):
         self.name = name
-        self.__findStatus = re.compile(r'\bTESTSTATUS\b')
-        self.__findText = re.compile(r'\bFTEXT\b')
+        self.__Status = re.compile(r'\bTESTSTATUS\b')
         self.__findImage = re.compile(r'\bCOMPIMAGE\b')
+        self.__findText = re.compile(r'\bFTEXT\b')
         self.__findIcon = re.compile(r'\bFICON\b')
 
     def check(self) -> bool:
         """ Check if function object is correctly created """
         check = False 
-        if self.__findStatus.match(self.name) or self.__findText.match(self.name) or self.__findImage.match(self.name) or self.__findIcon.match(self.name):
+        if self.__Status.match(self.name) or self.__findText.match(self.name) or self.__findImage.match(self.name) or self.__findIcon.match(self.name):
             check = True
         return check
     
     def evaluate(self):
-        if self.__findText.match(self.name):
+        if self.__Status.match(self.name):
+            print('Start')
+        elif self.__findImage.match(self.name):
+            print('Find Image')
+        elif self.__findText.match(self.name):
             word = ''
             print('Find Text')
             function = self.name.split('(')
@@ -30,7 +34,10 @@ class callFunction:
                         word+=i
             else: print('Error in Function Call')
             print(word)
-            
+        elif self.__findIcon.match(self.name):
+            print('Find icon')
+        else: print('Error in Function Call')
+
         
 
 
