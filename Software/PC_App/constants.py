2d2d1bfd5cd76d536360e87ac2e9e5e9d6a1e29a
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-05-27 01:34:24 -0600

    Create file to store all important constants in App

---
+++ Software/PC_App/constants.py
@@ -0,0 +1,10 @@
+################# File for handling all important constants ###################
+
+###### FIND OBJECT constants
+FOBJ_GRAY_THRESHOLD = 100             # Threshold for input image grayscale (0 - 255)
+FOBJ_RESIZE_INPUT_STD = (2400, 2400)  # Standard resize values for input image (x,y)
+FOBJ_RESIZE_TEMP_STD = 100            # Template resize (pixels)
+FOBJ_MATCH_THRESHOLD = 95             # Acceptance threshold for matching template
+FOBJ_ACCEPTANCE_DIFF = 30             # Acceptance difference between found objects
+FOBJ_MT_ITERATIONS = 100              # Iterations to apply Match template algorithm
+
