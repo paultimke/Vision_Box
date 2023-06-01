ce9e1968784fcb6b51f06e2621071f1a9dc86f23
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-06-01 04:15:56 -0600

    Added calibration tool GUI to select corners
    GUI to select the corners of the screen

---
+++ Software/PC_App/calibration_tool.py
@@ -0,0 +1,120 @@
+import cv2
+import numpy as np
+import json
+import constants as cnst
+from typing import List, Dict
+
+corner_t = List[int]
+
+############################### VARIABLES ################################
+# List of reference points
+ref_points = []
+
+########################### MAIN PROGRAM BODY ############################
+def write_corners_json(corners: List[corner_t]) -> None:
+    """ Updates config.json to new calibrated screen corners """
+    dic = {
+        "screen_corners": {
+            "topL": corners[0],
+            "topR": corners[1],
+            "botR": corners[2],
+            "botL": corners[3]
+        }
+    }
+
+    # Serializing json
+    json_obj = json.dumps(dic, indent=4)
+    # Writing to file
+    with open(cnst.CONFIG_FILE_NAME, "w") as outfile:
+        outfile.write(json_obj)
+
+
+def capture_frame(cam_obj) -> cv2.Mat:
+    """ Captures live camera frame """
+    #Load the image
+    frame = cv2.imread('Testing/screens_vbox_cam/T03_vbox_cam.png')
+    #ret, frame = cam_obj.read()
+    frame = cv2.rotate(frame, cv2.ROTATE_180)
+    return frame
+
+def click_Callback(event, x, y, flags, param):
+    """ Callback function on click event to append reference point """
+    # if left mouse button was clicked, record (x, y) coordinates 
+    if event == cv2.EVENT_LBUTTONDOWN:
+        ref_points.append([x,y])
+
+def draw_help_text(frame, n_saved_points):
+    """ Draws helpful text messages and visual feedback on screen """
+    # Draw help messages at the top of the screen
+    (x_start, y_start) = (15, 30)
+    COLOR_WHITE_BGR = (255, 255, 255)
+    FONT_SCALE = 1
+    msg = "Left click on the four screen corners in clockwise order\nPress \'s\' to save and quit | Press \'r\' to reset points"
+    dy = 30 # Spacing for newlines
+    for (i, line) in enumerate(msg.split('\n')):
+        y = y_start + i*dy
+
+        img = cv2.putText(frame, line, (x_start, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, fontScale=FONT_SCALE,
+                        color=COLOR_WHITE_BGR)
+        
+    # Draw text to count number of recorded points on bottom of frame
+    (x_start, y_start) = (x_start, frame.shape[0] - 30) # Get new y based on frame size
+    points_msg = f"Recorded points: {n_saved_points}"
+    img = cv2.putText(frame, points_msg, (x_start, y_start),
+                      cv2.FONT_HERSHEY_SIMPLEX, fontScale=FONT_SCALE,
+                      color=COLOR_WHITE_BGR)
+    return img
+
+def main():
+    # load the image, rotate it to face forwards and clone it
+    vid = cv2.VideoCapture(0)
+    image = capture_frame(vid)
+    clone = image.copy()
+
+    cv2.namedWindow("Vision Box Calibration Tool")
+    cv2.setMouseCallback("Vision Box Calibration Tool", click_Callback)
+
+    # keep looping until the 'q' key is pressed
+    while True:
+        # display the image and wait for a keypress
+        image = capture_frame(vid)
+        #cv2.imshow("Vision Box Calibration Tool", image)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if there are two reference points, then draw polygon to 
+        # wait for user feedback
+        if len(ref_points) == 4:
+            npy_ref_points = np.array(ref_points, np.int32)
+            npy_ref_points = npy_ref_points.reshape((-1, 1, 2))
+            cv2.polylines(image, [npy_ref_points], 
+                          isClosed=True, color=(0,255,0), thickness=2)
+
+        # if the 'r' key is pressed, reset the cropping region
+        if key == ord("r"):
+            image = clone.copy()
+            ref_points.clear()
+
+        # if the 'c' key is pressed, break from the loop
+        if key == ord("s"):
+            if len(ref_points) == 4:
+                # Log to terminal
+                print("Saved points are: ")
+                print(f"Top-Left: {ref_points[0]}")
+                print(f"Top-Right: {ref_points[1]}")
+                print(f"Bottom-Left: {ref_points[2]}")
+                print(f"Bottom-Right: {ref_points[3]})")
+
+                # Modify configuration file
+                print("\nUpdated configuration file config.json")
+                write_corners_json(ref_points)
+            break
+
+        image = draw_help_text(image, len(ref_points))
+        cv2.imshow("Vision Box Calibration Tool", image)
+
+    # close all open windows
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
