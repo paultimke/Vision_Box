b9b42336df3eda62b5db5e622495c947c2836501
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-06-03 14:57:51 -0700

    Update main.py to use threads

--- Software/PC_App/main.py
+++ Software/PC_App/main.py
@@ -1,44 +1,149 @@
-# Import Libraries
-from typing import List
+import threading
 import sys
-import constants as cnst
+import time
 import takeFunction as parser
 import find_object as fOBJ
+import constants as cnst
+from vbox_logger import logger_init
 
-# Flag indicating if debugg mode
-DEBG = False
+############################### Global variables ##############################
+debug_flag = False
+cmd_queue = []
+mutex = threading.Lock()
+logger = logger_init()
 
-def is_CLI_args_valid(args: List[str]) -> bool:
-    return len(args) == 2
-
-#-------------------MAIN FUNCTION-------------------#
-def main():  
-    # Get command line arguments
+########################### Main Program Functions ############################
+def main():
+    # Get command line args
     command_args = None
     if is_CLI_args_valid(sys.argv):
         command = parser.callFunction(sys.argv[1])
-        if command.check():
-            command_args = command.parse()
-
-    # Initialize cam port
-    cam_port = "Testing/screens_cam18/T03_cam_18.png" #''cnst.DEFAULT_CAM_PORT
+        command_args = command.parse()
+        if command_args[-1] == "-debug":
+            debug_flag = True
+    else:
+        print(sys.argv)
+        print("Must specify one command to run")
+        exit()
 
     # Call corresponding command
     match command_args:
-        # TODO: Add suport for HELP, EXAMPLE and SETLIGHT commands
         case ('TESTSTATUS', condition):
-            pass
-        case ('FTEXT', text):
-            pass
-        case ('FICON', path):
-            # Acceptance match threshold before refactor was on 50, although
-            # the default is 95
-            fOBJ.mainly(cam_port, path, DEBG)
-        case ('COMPIMAGE', path):
-            pass
+            # Start CLI and command-servicing threads
+            cli = threading.Thread(target=CLI_handler, args=(condition, ))
+            cmd_server = threading.Thread(target=process_command, daemon=True)
+            cli.start()
+            cmd_server.start()
+            cli.join()
+        case (cmd, arg):
+            print(f"Processing command {cmd}({arg})")
+            #process_command(cmd, arg)
         case _:
-            # Log error message of unrecognized command
+            print("Unknown Error")
             pass
+# END main()
 
-if __name__ == "__main__":
+
+def CLI_handler(condition: str):
+    """ Thread to handle user CLI input """
+    end = False
+    if condition == 'START':
+        print("\nTEST STARTED")
+        while not end:
+            # Get string input from terminal
+            input_str = input("[PC] >> ")
+            command = parser.callFunction(input_str)
+            command_args = command.parse()
+
+            # Log received command to log file
+            logger.info(f"[PC] {command_args[0]}({command_args[1]})")
+
+            # Append command to queue
+            match command_args:
+                case ('TESTSTATUS', 'START'):
+                    logger.error("Can't start new TEST, one is already running") 
+                case ('TESTSTATUS', 'END'):
+                    end = True
+                case _:
+                    with mutex:
+                        if len(cmd_queue) > 0:
+                            logger.info("[VB] Command in queue")
+                        cmd_queue.append(command_args)
+    else:
+        # TESTSTATUS(END) was called before starting a TEST
+        logger.error("Can not end a not started TESTSTATUS")
+# END CLI_handler()
+
+def process_command():
+    """ Thread to handle command processing. It takes commands\
+        out of the common cmd_queue """
+    def execute_command(cmd, arg):
+        match (cmd, arg):
+                case ('FICON', x):
+                    time.sleep(7)
+                    #log
+                case ('FTEXT', x):
+                    time.sleep(7)
+                    #log
+                case ('COMPIMAGE', x):
+                    time.sleep(7)
+                    #log
+                case _:
+                    pass
+            
+    # Commands were given directly and no concurrency is happening
+    if (cmd, arg) != (None, None):
+        execute_command(cmd, arg)
+
+    # TESTSTATUS was started, and cmd_queue is being filled by other thread
+    commands_pending = False
+    while True:
+        with mutex:
+            if len(cmd_queue) > 0:
+                (cmd, arg) = cmd_queue.pop(0)
+                commands_pending = True
+        
+        if commands_pending:
+            commands_pending = False
+            execute_command(cmd, arg)
+# END process_command()
+            
+
+def is_CLI_args_valid(args):
+    """ Checks CLI input args are of valid length taking\
+          Powershell and Command Prompt differences into account """
+    valid = False
+    # Powershell takes parentheses differently as command prompt and already 
+    # separates input. Hence, correct command will have three values. Concatenate
+    # them to treat them equally as cmd prompt
+    def match_PS_cmdPrompt_formats():
+        tmp_args = sys.argv
+        sys.argv.clear()
+        sys.argv.append(tmp_args[0])
+        sys.argv.append(f"{tmp_args[1]}({tmp_args[2]})")
+
+    match(len(args)):
+        case 4:
+            match_PS_cmdPrompt_formats()
+            if args[3] == '-debug':
+                valid = True
+                match_PS_cmdPrompt_formats()
+            else:
+                # TODO: Log error to log file
+                print(f"Error: Unknown argument {args[3]}")
+        case 3:
+            valid = True
+            # 3rd argument could be Debug flag on cmd_prompt or command ran in powershell
+            if args[2] != '-debug':
+                # TODO: Log error to log file
+                print(f"Error: Unknown argument {args[3]}")
+        case 2:
+            valid = True
+        case _:
+            # TODO: Log error to log file
+            print(f"Error: Provide argument in the form COMMAND(ARG)")
+    return valid
+            
+############################# PROGRAM ENTRY POINT #############################
+if __name__ == '__main__':
     main()
