5dda42396edc6e6698d406ca89469fde9ef34f1f
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-06-04 22:27:17 -0700

    Add lookup table with command callbacks
    Added a lookup table with command names as keys and callbacks to corresponding functions as values as a way to select appropriate function based on input

--- Software/PC_App/main.py
+++ Software/PC_App/main.py
@@ -2,14 +2,26 @@ import threading
 import sys
 import time
 import takeFunction as parser
-import find_object as fOBJ
 import constants as cnst
+import find_object as fOBJ
+from comp_img import CompareImage
+from detect_text import find_text
+from set_light import Set_light
 from vbox_logger import logger
 
 ############################### Global variables ##############################
 debug_flag = False
 cmd_queue = []
 mutex = threading.Lock()
+
+cmd_lookup_table = {
+    'FICON': fOBJ.mainly,
+    'FTEXT': find_text,
+    'COMPIMAGE': CompareImage,
+    'SETLIGHT': Set_light,
+    'HELPME': None,   #TODO
+    'EXAMPLE': None   #TODO
+}
 
 ########################### Main Program Functions ############################
 def main():
@@ -36,18 +48,18 @@ def main():
             cli.join()
         case (cmd, arg):
             print(f"Processing command {cmd}({arg})")
-            #process_command(cmd, arg)
+            process_command(cmd, arg)
         case _:
             print("Unknown Error")
             pass
 # END main()
-
 
 def CLI_handler(condition: str):
     """ Thread to handle user CLI input """
     end = False
     if condition == 'START':
         print("\nTEST STARTED")
+        logger.info(f"[PC] TESTSTATUS(START)")
         while not end:
             # Get string input from terminal
             input_str = input("[PC] >> ")
@@ -73,22 +85,12 @@ def CLI_handler(condition: str):
         logger.error("Can not end a not started TESTSTATUS")
 # END CLI_handler()
 
-def process_command():
+def process_command(cmd=None, arg=None):
     """ Thread to handle command processing. It takes commands\
         out of the common cmd_queue """
     def execute_command(cmd, arg):
-        match (cmd, arg):
-                case ('FICON', x):
-                    time.sleep(7)
-                    #log
-                case ('FTEXT', x):
-                    time.sleep(7)
-                    #log
-                case ('COMPIMAGE', x):
-                    time.sleep(7)
-                    #log
-                case _:
-                    pass
+        print(f"[VB] >> ACK {cmd}({arg})")
+        #cmd_lookup_table[cmd](arg)
             
     # Commands were given directly and no concurrency is happening
     if (cmd, arg) != (None, None):
@@ -109,10 +111,8 @@ def process_command():
             
 
 def is_CLI_args_valid(args):
-    """ Checks CLI input args are of valid length taking\
-          Powershell and Command Prompt differences into account """
     valid = False
-    # Powershell takes parentheses differently as command prompt and already 
+    # Powershell takes parentheses differently than command prompt and already 
     # separates input. Hence, correct command will have three values. Concatenate
     # them to treat them equally as cmd prompt
     def match_PS_cmdPrompt_formats():
@@ -135,14 +135,15 @@ def is_CLI_args_valid(args):
             valid = True
             # 3rd argument could be Debug flag on cmd_prompt or command ran in powershell
             if args[2] != cnst.CLI_DEBUG_FLAG_NAME:
-                match_PS_cmdPrompt_formats
+                match_PS_cmdPrompt_formats()
         case 2:
             valid = True
         case _:
             # TODO: Log error to log file
             print(f"Error: Provide argument in the form COMMAND(ARG)")
     return valid
+# END is_CLI_args_valid()
             
-############################# PROGRAM ENTRY POINT #############################
+
 if __name__ == '__main__':
     main()
