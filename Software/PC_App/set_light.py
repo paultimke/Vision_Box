19576435904b14c78a1b002a7b1fdc0bf2c4f0bc
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-06-05 02:51:39 -0600

    Update set_light to be more platform independent
    Serial tools may behave a bit differently on Windows and POSIX systems. Added buffer flushes, time delays and reversed port lists to make system faster and more robust on both platforms

--- Software/PC_App/set_light.py
+++ Software/PC_App/set_light.py
@@ -4,26 +4,37 @@ import time
 from vbox_logger import logger
 
 class light_controller:
+    LOG_TAG = "SETLIGHT"
+    DELAY_TIME = 0.2
+
     def __init__(self):
         """ Light Controller object constructor\
             Establishes serial port connection to MCU """
         self.setup_complete = False
 
         # Automatically looks for device in listed ports
+        # Looks first on last ports as they are more likely
+        # to be the light controller device
         ports = serial.tools.list_ports.comports()
+        ports.reverse()
         for port in ports:
             try:
                 self._ser = serial.Serial(port.device, 
-                                         baudrate=19200,
+                                         baudrate=38400,
                                          timeout=0.5)
                 
                 # Send dummy byte and wait for response to ensure
                 # connection with real light controller device was made
+                time.sleep(self.DELAY_TIME)
                 DUMMY_BYTES = b'255'
+                self._ser.reset_output_buffer()
+                self._ser.reset_input_buffer()
                 self._ser.write(DUMMY_BYTES)
                 if self._wait_mcu_ack(DUMMY_BYTES):
                     self.setup_complete = True
-                    logger.debug(f"[VB]: Connection to {port.device} successful")
+                    logger.debug("VB", f"Connection to {port.device} successful", 
+                                 tag=self.LOG_TAG)
+                    break
                 else:
                     # If ACK was not received, close connection with
                     # connected serial port
@@ -32,24 +43,28 @@ class light_controller:
                 continue
         
         if not self.setup_complete:
-            logger.warning("[VB]: Could not establish a connection with " 
-                  "Light Controller Hardware")
+            logger.warning("VB", "Could not establish a connection with " 
+                  "Light Controller Hardware", tag=self.LOG_TAG)
 
     def detach(self):
         """ Light Controller object destructor\
             Closes serial port connection with MCU """
         if self.setup_complete:
             self._ser.close()
-            logger.debug(f"[VB]: Connection with {self._ser.port} closed")
+            logger.debug("VB", f"Connection with {self._ser.port} closed", 
+                         tag=self.LOG_TAG)
 
     def set_brightness(self, luxes: int):
         """ Command to set Vision Box light brightness level"""
         if self.setup_complete:
             tx_bytes = self._serialize_luxes(luxes)
+            self._ser.reset_output_buffer()
+            self._ser.reset_input_buffer()
             self._ser.write(tx_bytes)
             # Confirm data sent was received well
             if self._wait_mcu_ack(tx_bytes):
-                logger.info(f"[VB]: Light brightness succesfully set to {luxes} lux")
+                logger.info("VB", f"Light brightness succesfully set to {luxes} lux",
+                            tag=self.LOG_TAG)
 
     def _serialize_luxes(self, luxes: int):
         """ Converts int lux value into corresponding string to be sent\
@@ -65,7 +80,7 @@ class light_controller:
             and False otherwise.
             Data echoed by MCU must match format RX<NL> where X is a number"""
         # Give time for light controller device to respond
-        time.sleep(0.5)
+        time.sleep(self.DELAY_TIME)
 
         # Wait for acknowledge and verify data format
         valid = False
