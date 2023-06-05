f2e202c3b4308d75ce0f3483e7e8ff187ba50d9f
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-06-04 20:56:37 -0700

    Add automatic line counting on logger
    A wrapper around python's own logging module was made to increment line numbers automatically on each call to a logging function. A function to print a preamble to log file was also added.

--- Software/PC_App/vbox_logger.py
+++ Software/PC_App/vbox_logger.py
@@ -1,14 +1,61 @@
 import logging
 
-def logger_init(filename='vbox.log', debug=False):
-    logger = logging.getLogger('vbox')
-    fh = logging.FileHandler(filename)
-    fh_Formatter = logging.Formatter('[%(asctime)s] - %(message)s',
-                                     datefmt='%Y-%m-%d-%H-%M-%S')
-    fh.setFormatter(fh_Formatter)
-    logger.addHandler(fh)
-    if debug:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-    return logger
+def _configure_logger():
+    return VBOX_logger()
+
+class VBOX_logger:
+    """ Wrapper around python's logging module to add specific features like\
+        line number counting """
+    
+    def __init__(self, filename='vbox.log'):
+        self.curr_line_num = 0
+        self.debug_flag = False
+        self.logger = logging.getLogger('vbox')
+        self.fileHandler = logging.FileHandler(filename)
+        fh_Formatter = logging.Formatter('%(message)s')
+        self.fileHandler.setFormatter(fh_Formatter)
+        self.logger.addHandler(self.fileHandler)
+
+        # The flag logging.DEBUG is no longer needed since debug printing
+        # is now handled through the self.debug_flag variable
+        self.logger.setLevel(logging.INFO)
+    
+    def preamble_log(self):
+        self.logger.info("VISION BOX LOG FILE")
+        self.logger.info("Starting light level: 0")
+        self.logger.info(f"Verbose Output enabled: {self.debug_flag}\n")
+
+        TS_PADDING = 21 - len('TIMESTAMP') # TIMESTAMP uses 21 char columns
+        self.logger.info(f"TIMESTAMP{' '*TS_PADDING}  #  who  DESCRIPTION")
+
+        fh_Formatter = logging.Formatter('[%(asctime)s]  %(message)s',
+                                        datefmt='%Y-%m-%d-%H-%M-%S')
+        self.fileHandler.setFormatter(fh_Formatter)
+        self.logger.addHandler(self.fileHandler)
+
+    def enable_debug_level(self):
+        self.debug_flag = True
+
+    def info(self, msg):
+        msg = f"{self.curr_line_num} {msg}"
+        self.logger.info(msg)
+        self.curr_line_num += 1
+
+    def debug(self, msg):
+        if self.debug_flag:
+            msg = f"{self.curr_line_num} {msg}"
+            self.logger.debug(msg)
+            self.curr_line_num += 1
+
+    def warning(self, msg):
+        msg = f"{self.curr_line_num} {msg}"
+        self.logger.warning(msg)
+        self.curr_line_num += 1
+
+    def error(self, msg):
+        msg = f"{self.curr_line_num} {msg}"
+        self.logger.error(msg)
+        self.curr_line_num += 1
+
+if __name__ == 'vbox_logger':
+    logger = _configure_logger()
