d1de146de4b462a581d42fe43900faa204d541b5
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-06-06 12:56:55 -0600

    Add output images dir name to constants

--- Software/PC_App/constants.py
+++ Software/PC_App/constants.py
@@ -8,6 +8,7 @@ SOFTWARE_VERSION = f"{SW_VERSION_MAJOR}.{SW_VERSION_MINOR}.{SW_VERSION_REV}"
 
 DEFAULT_CAM_PORT = '0'               # Default USB port for camera
 DEFAULT_LOG_FILE_NAME = "vbox.log"    # Default file name for log
+OUTPUT_IMAGES_DIR = "output_images"   # Output images directory
 CONFIG_FILE_NAME = "config.json"      # File name for configuration
 CLI_DEBUG_FLAG_NAME = "-verbose"      # CLI argument name to trigger verbose output
 
