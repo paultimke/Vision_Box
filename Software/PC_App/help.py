a7e9424da9153322b55eedab56b3c74a69e78a74
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-06-07 13:35:38 -0700

    Add example command

--- Software/PC_App/help.py
+++ Software/PC_App/help.py
@@ -23,7 +23,7 @@ def help_cmd(_, x):
     cmd = "COMPIMAGE(\"path\")"
     print(f"{cmd:<{MAX_CMD_WIDTH}}", "where path is the path to a reference image to compare")
     cmd = "SETLIGHT(range)" 
-    print(f"{cmd:<{MAX_CMD_WIDTH}}", "where range is an integer range of luxes to set brightness")
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "where range is an integer range (0-100) to set brightness")
     cmd = "HELPME" 
     print(f"{cmd:<{MAX_CMD_WIDTH}}", "help information about the usage of commands")
     cmd = "EXAMPLE" 
@@ -39,4 +39,32 @@ def help_cmd(_, x):
 # Wildcard _ is added as arg althought none is used, as every command must 
 # have the same signature for program compatibility
 def example_cmd(_, x):
+    """ Prints help information to console about command usage """
+    PADDING = 2
+    MAX_CMD_WIDTH = len("COMPIMAGE(\"path\")") + PADDING
+
+    print(f"Vision Box Software {cnst.SOFTWARE_VERSION}\n")
+    print("Usage: vbox [Commands] [Options]\n")
+
+    # Commands
+    print("Commands")
+    cmd = "TESTSTATUS(cond)" 
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "TESTSTATUS(START) or TESTSTATUS(END)")
+    cmd = "FICON(\"path\")"
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "FICON(\"assets/small_icon.png\")")
+    cmd = "FTEXT(\"text\")"
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "FTEXT(\"hello\")")
+    cmd = "COMPIMAGE(\"path\")"
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "COMPIMAGE(\"Frames/screen_01/png\")")
+    cmd = "SETLIGHT(range)" 
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "SETLIGHT(75) sets light to 75 percent brightness")
+    cmd = "HELPME" 
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "HELPME")
+    cmd = "EXAMPLE" 
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "EXAMPLE")
+
+    # Options
+    print("\nOptions")
+    cmd = "-verbose"
+    print(f"{cmd:<{MAX_CMD_WIDTH}}", "TESTSTATUS(START) -verbose")
     pass
